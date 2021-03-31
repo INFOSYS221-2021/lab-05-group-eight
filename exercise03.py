2890207099c3e360c79a5f003e74f4ff5af21af0
william-leyang <79950819+william-leyang@users.noreply.github.com>
2021-03-31 14:45:25 +1300

    completed ex 3

--- exercise03.py
+++ exercise03.py
@@ -1,4 +1,10 @@
 # Exercise Three
 # Write a simple program that finds the number of digits of a given integer value
+value = -100
+valStr = str(value)
+if valStr.isdigit():
+    print(len(valStr))
+else:
+    print(len(valStr)-1)
 
 print("The number of digits for a value of 100 is 3")
