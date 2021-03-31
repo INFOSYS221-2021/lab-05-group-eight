8b460b31f034b78dcf1f463e3d371a81785d9092
william-leyang <79950819+william-leyang@users.noreply.github.com>
2021-03-31 14:32:10 +1300

    completed ex 2

--- exercise02.py
+++ exercise02.py
@@ -1,4 +1,14 @@
 # Exercise Two
 # Write a simple program that finds the value at the nth position in the Fibonacci sequence
-
+n1 = 0
+n2 = 1
+count = 1
+nterms = 6
+nth = 0
+while count < nterms:
+    nth = n1 + n2
+    n1 = n2
+    n2 = nth
+    count += 1
+print(nth)
 print("The value at the 6th postion in the Fibonacci sequence is 8")
